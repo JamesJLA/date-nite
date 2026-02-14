3fe063c8e91d95f0872371736ffba6d224aeee10
JamesJLA <james.alexander9204@gmail.com>
2026-02-13 21:29:16 -0500

    auth settings

--- planner/urls.py
+++ planner/urls.py
@@ -1,5 +1,11 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeDoneView,
+    PasswordChangeView,
+)
 from django.urls import path
+from django.urls import reverse_lazy
 
 from .views import HomeView, ResultsView, SignUpView, VoteView
 
@@ -15,6 +21,21 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "settings/password/",
+        PasswordChangeView.as_view(
+            template_name="planner/password_change.html",
+            success_url=reverse_lazy("planner:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "settings/password/done/",
+        PasswordChangeDoneView.as_view(
+            template_name="planner/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
     path("", HomeView.as_view(), name="home"),
     path("vote/<uuid:token>/", VoteView.as_view(), name="vote"),
     path("results/<uuid:token>/", ResultsView.as_view(), name="results"),
